64cde7e83bd4bb7df2a4806bb4d1aa108cebae67
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2025-06-25 00:42:03 +0100

    feat: begin c# project, implement iplugin2 interface

---
+++ AcerPlugin.cs
@@ -0,0 +1,28 @@
+﻿using FanControl.Plugins;
+
+namespace FanControl.Acer_PO3_640
+{
+    public class AcerPlugin : IPlugin2
+    {
+        public string Name => "Acer-PO3-640";
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+        public void Load(IPluginSensorsContainer _container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
