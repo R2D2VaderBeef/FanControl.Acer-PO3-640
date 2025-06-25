0c2230315b5f991630ee441cf5f5af5e73a40813
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2025-06-25 15:19:27 +0100

    fix: make dll exports discoverable

--- ECLibrary/dllmain.cpp
+++ ECLibrary/dllmain.cpp
@@ -3,39 +3,54 @@
 #include "ec.hpp"
 
 EmbeddedController ec;
+BOOL ready = false;
 
 extern "C" BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        ec = EmbeddedController();
-        ec.endianness = BIG_ENDIAN;
-        if (ec.driverFileExist && ec.driverLoaded) {
-            return TRUE;
-        }
-        else {
-            return FALSE;
-        }
-        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
-        break;
     case DLL_PROCESS_DETACH:
-        ec.close();
         break;
     }
     return TRUE;
 }
 
-extern "C" WORD ReadWord(BYTE _register) {
-    return ec.readWord(_register);
-}
-
-extern "C" BYTE WriteWord(BYTE _register, WORD _value) {
-    bool result = ec.writeWord(_register, _value);
-    if (result == true) {
-        return 0x01;
+extern "C" {
+    __declspec(dllexport) void Setup() {
+        ec = EmbeddedController();
+        ec.endianness = BIG_ENDIAN;
+        if (ec.driverFileExist && ec.driverLoaded) {
+            ready = TRUE;
+        }
+        else {
+            ready = FALSE;
+        }
     }
-    else return 0x00;
+
+    __declspec(dllexport) void Shutdown() {
+        if (ready == TRUE) {
+            ec.close();
+        }
+    }
+
+    __declspec(dllexport) WORD ReadWord(BYTE _register) {
+        if (ready == TRUE) {
+            return ec.readWord(_register);
+        }
+        else return 0x0000;
+    }
+
+    __declspec(dllexport) BYTE WriteWord(BYTE _register, WORD _value) {
+        if (ready == TRUE) {
+            bool result = ec.writeWord(_register, _value);
+            if (result == true) {
+                return 0x01;
+            }
+            else return 0x00;
+        }
+        else return 0x00;
+    }
 }
