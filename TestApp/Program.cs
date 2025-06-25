3706bf1ef6a6d4541d1964af2982d0dbf01c8866
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2025-06-25 15:19:47 +0100

    feat: cli app for testing

---
+++ TestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+partial class TestClass
+{
+    [LibraryImport("ECLibrary.dll")]
+    private static partial ushort ReadWord(byte _register);
+
+    [LibraryImport("ECLibrary.dll")]
+    private static partial byte WriteWord(byte _register, ushort _value);
+
+    [LibraryImport("ECLibrary.dll")]
+    private static partial void Setup();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Loading DLL");
+        NativeLibrary.Load(Path.Combine(Directory.GetCurrentDirectory(), "Plugins\\Acer-PO3-640\\ECLibrary.dll"));
+        
+        Console.WriteLine("Trying init method");
+        Setup();
+
+        Console.WriteLine("Trying read method");
+        ushort result = ReadWord((byte) 0x14);
+        Console.WriteLine(result);
+    }
+}
