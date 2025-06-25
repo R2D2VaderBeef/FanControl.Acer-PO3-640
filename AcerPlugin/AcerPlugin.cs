f579a62fc700edb80c7ae6c24d7ba1957719ecf2
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2025-06-25 13:25:27 +0100

    feat: base loadable fancontrol plugin, register fake sensors

--- AcerPlugin/AcerPlugin.cs
+++ AcerPlugin/AcerPlugin.cs
@@ -1,28 +1,67 @@
 ﻿using FanControl.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security.Principal;
 
 namespace FanControl.Acer_PO3_640
 {
-    public class AcerPlugin : IPlugin2
+    [SupportedOSPlatform("windows")]
+    public partial class AcerPlugin : IPlugin2
     {
+        [LibraryImport("ECLibrary.dll")]
+        private static partial ushort ReadWord(byte _register);
+        
+        [LibraryImport("ECLibrary.dll")]
+        private static partial byte WriteWord(byte _register, ushort _value);
+
         public string Name => "Acer-PO3-640";
+        private readonly IPluginDialog dialog;
+        private readonly IPluginLogger logger;
+        private bool failed = false;
+
+        public AcerPlugin(IPluginLogger logger, IPluginDialog dialog)
+        {
+            this.logger = logger;
+            this.dialog = dialog;
+        }
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+            logger.Log("[Acer-PO3-640][Debug] Initialising...");
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                logger.Log("[Acer-PO3-640][Debug] Administrator permission confirmed");
+                Marshal.PrelinkAll(typeof(AcerPlugin));
+            }
+            else
+            {
+                logger.Log("[Acer-PO3-640] Error: Not Run as Administrator.");
+                dialog.ShowMessageDialog("Error - Acer-PO3-640 requires you to run FanControl as Administrator.");
+                failed = true;
+            }
         }
-        public void Load(IPluginSensorsContainer _container)
+        public void Load(IPluginSensorsContainer container)
         {
-            throw new NotImplementedException();
+            if (failed == true) return;
+            container.FanSensors.Add(new FakeSensor("po3-640-cpu", "CPU Fan", 2500));
+            container.TempSensors.Add(new FakeSensor("po3-640-faketemp", "Fake Temperature", 2500));
+            logger.Log("[Acer-PO3-640][Debug] Loaded");
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            if (failed == true) return;
+            logger.Log("[Acer-PO3-640][Debug] Shutting Down");
         }
 
         public void Update()
         {
-            throw new NotImplementedException();
+            if (failed == true) return;
         }
     }
 }
---
+++ AcerPlugin/FakeSensor.cs
@@ -0,0 +1,26 @@
+﻿using FanControl.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FanControl.Acer_PO3_640
+{
+    public class FakeSensor : IPluginSensor
+    {
+        internal FakeSensor(string id, string name, float value)
+        {
+            Id = id;
+            Name = name;
+            Value = value;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+
+        public float? Value { get; internal set; }
+        public void Update() { }
+    }
+}
